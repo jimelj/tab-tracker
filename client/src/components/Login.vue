4dc1b3792eea9fa3aa38ff6fb0348eab11f3cb0a
Jimel Joseph <jimelj@gmail.com>
2017-12-13 10:51:48 -0500

    login complete

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,13 +3,10 @@
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
+          <v-toolbar-title>Login</v-toolbar-title>
         </v-toolbar>
 
         <div class="pl-4 pr-4 pt-2 pb-2">
-        <form
-          name="tab-tracker-form"
-          autocomplete="off">
         <v-text-field
               label="Email"
               v-model="email"
@@ -19,16 +16,14 @@
               label="Password"
               type="password"
               v-model="password"
-              autocomplete="new-password"
             ></v-text-field>
-        </form>
           <br>
           <div class="error" v-html="error" />
           <br>
           <v-btn class="cyan"
             dark
-            @click="register">
-            Register
+            @click="login">
+            Login
           </v-btn>
         </div>
       </div>
@@ -47,9 +42,9 @@ export default {
     }
   },
   methods: {
-    async register () {
+    async login () {
       try {
-        await AuthenticationService.register({
+        await AuthenticationService.login({
           email: this.email,
           password: this.password
         })
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,8 +1,57 @@
-module.exports = (sequelize, DataTypes) =>
-  sequelize.define('User', {
+const Promise = require('bluebird')
+const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'))
+
+function hashPassword (user, options) {
+  const SALT_FACTOR = 8
+
+  if (!user.changed('password')) {
+    return
+  }
+
+  return bcrypt
+    .genSaltAsync(SALT_FACTOR)
+    .then(salt => bcrypt.hashAsync(user.password, salt, null))
+    .then(hash => {
+      user.setDataValue('password', hash)
+      console.log(`hash${hash}`)
+    })
+}
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     email: {
       type: DataTypes.STRING,
       unique: true
     },
     password: DataTypes.STRING
+  }, {
+    hooks: {
+      beforeCreate: hashPassword,
+      beforeUpdate: hashPassword,
+      beforeSave: hashPassword
+    }
   })
+
+  User.prototype.comparePassword = function (password) {
+    console.log(`PLAIN ${password}`)
+    console.log(`this.password ${this.password}`)
+    return bcrypt.compareAsync(password, this.password)
+
+    // bcrypt.compare(password, this.password, function (err, res) {
+    //   if (err) {
+    //     console.error(err)
+    //   } else {
+    //     console.log(`ressponse ${res}`)
+    //     return res
+    //   }
+    // })
+
+    // bcrypt.compareAsync(password, this.password).then(function (res) {
+    //   console.log(`ressponse ${res}`)
+    //   console.log('hi')
+    //   return res
+    // })
+  }
+
+  return User
+}
